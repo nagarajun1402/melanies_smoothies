7fc3bfb71d7b9763bae79c8eea09dfcc3c6cbde4
nagarajun1402 <nagarajun1402@gmail.com>
2026-05-22 22:44:47 +0530

    Made changes to requests
    Made changes to requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import requests
 
 
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw: ")
 st.write(
