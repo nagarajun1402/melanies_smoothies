1989d323a27373e2a3c2cdb3b68ec21520d12dd5
nagarajun1402 <nagarajun1402@gmail.com>
2026-05-23 12:34:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your smoothie is ordered! {name_on_order}", icon='✅')
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+# smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
