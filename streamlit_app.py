273eaa98c8710ad283f533e2b55f072c88d3dfff
nagarajun1402 <nagarajun1402@gmail.com>
2026-05-23 12:40:22 +0530

    Refactor smoothie ordering and remove comments
    Refactor smoothie order logic and clean up commented code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     # st.write(ingredient_string)
     
@@ -49,6 +51,6 @@ if ingredients_list:
         st.success(f"Your smoothie is ordered! {name_on_order}", icon='✅')
 
 # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
